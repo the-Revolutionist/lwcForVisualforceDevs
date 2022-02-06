6cfe14e9970f95e4eb11861e215bcd65d8a6d1d4
the-Revolutionist <therevolutionw7@gmail.com>
2022-02-06 18:50:01 -0500

    wrapped up testing

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
-const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
+const { jestConfig } = require("@salesforce/sfdx-lwc-jest/config");
 
 module.exports = {
-    ...jestConfig,
-    modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
+  ...jestConfig,
+  modulePathIgnorePatterns: ["<rootDir>/.localdevserver"],
+  moduleNameMapper: {
+    "^lightning/button$":
+      "<rootDir>/force-app/test/jest-mocks/lightning/button",
+    "^thunder/hammerButton$":
+      "<rootDir>/force-app/test/jest-mocks/thunder/hammerButton",
+    "^c/displayPanel$": "<rootDir>/force-app/test/jest-mocks/c/displayPanel"
+  }
 };
